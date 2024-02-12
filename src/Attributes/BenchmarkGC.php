0476e904739b0b86a48cefc7baeba7878f3f3929
N0zzy <ffnn83@yandex.ru>
2024-02-12 20:37:43 +0500

    0.2.0 refactoring and data correction

--- src/Attributes/BenchmarkGC.php
+++ src/Attributes/BenchmarkGC.php
@@ -1,5 +1,14 @@
 <?php
-
+/**
+ * PhpBenchmark
+ * @author N0zzy <https://github.com/N0zzy>
+ * @version 0.2 2023-02-12
+ * @link https://github.com/N0zzy/PhpBenchmark
+ * @license MIT
+ * @license https://github.com/N0zzy/PhpBenchmark/blob/master/LICENSE
+ * @copyright N0zzy
+ * @since 0.2
+ */
 namespace N0zzy\PhpBenchmark\Attributes;
 #[\Attribute]
 class BenchmarkGC
